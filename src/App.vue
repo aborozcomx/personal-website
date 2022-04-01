55ae0b36f5a50566be3b1cdab1777ef0025735c8
Adrian Bautista <adrianbautistaorozco@gmail.com>
2022-04-01 14:33:05 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <main>
+    <div class="card">
+        <img :src="personalInformation.image" :alt="personalInformation.username">
+        <h1>{{ personalInformation.username }}</h1>
+        <h4>{{ personalInformation.description }}</h4>
+      </div>
+  </main>
+</template>
+
+<script setup>
+import logo from '@/assets/logo.jpg'
+import { reactive } from 'vue'
+
+const personalInformation = reactive({
+  username: '@aborozcomx',
+  description: 'Frontend Develop at Plerk',
+  image: logo
+});
+
+</script>
+
+<style scope>
+@import './assets/base.css';
+
+main{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.card{
+  border-radius: 12px;
+  background: white;
+  padding: 10px;
+  width: 468px;
+}
+
+.card img{
+  border-radius: 50%;
+  display: block;
+  margin: -50px auto 0;
+  width: 85px;
+  height: 85px;
+}
+h1, h4{
+  text-align: center;
+}
+
+@media screen and (max-width: 468px){
+  .card{
+    width: 320px;
+  }
+}
+</style>
